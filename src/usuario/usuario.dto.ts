3de687d4029f207e5030c7a8c92d6b2601585532
SrGinko <91520452+SrGinko@users.noreply.github.com>
2025-04-29 13:33:37 -0300

    atualização do usuario

--- src/usuario/usuario.dto.ts
+++ src/usuario/usuario.dto.ts
@@ -1,8 +1,11 @@
 export interface Usuario{
     id: string;
     username: string;
+    email: string;
+    senha?:string;
     foto: string;
     nivel: number;
     xp : number;
     wallpaper: number;
+    quatidadeMensagens?:number;
 }
