2d0877cc14458d383a3d14a39236dd4716b52998
SrGinko <pedeohenriquecardoso@gmail.com>
2026-06-21 16:32:01 -0300

    feat: atualizar interface Projeto para permitir imagens e tecnologias opcionais

--- src/projetos/projetos.dto.ts
+++ src/projetos/projetos.dto.ts
@@ -4,7 +4,22 @@ export interface Projeto {
     url: string;
     descricao: string;
     emProgresso: boolean;
-    imagens: string[];
+    imagens?: ImagemProjetos[];
     dataCriacao?: Date;
     dataAtualizacao?: Date;
+    tecnologias?: Tecnologia[];
+}
+
+export interface ImagemProjetos{
+    id: number;
+    url: string;
+    descricao: string;
+    nomeImagem: string;
+}
+
+export interface Tecnologia {
+    id: number;
+    nome: string;
+    nivel: string;
+    principal: boolean;
 }
